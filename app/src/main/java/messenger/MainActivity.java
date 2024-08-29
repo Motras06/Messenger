90ff5aba0a9ce56a86b9425aaabe25fc8f27efb6
Motras <154556860+Motras06@users.noreply.github.com>
2024-08-30 02:05:52 +0300

    work 30.08.24 2:05

--- app/src/main/java/messenger/MainActivity.java
+++ app/src/main/java/messenger/MainActivity.java
@@ -1,5 +1,6 @@
 package messenger;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -11,6 +12,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+
+import messenger.ui.AutorizationFragment;
 import messenger.ui.ChatsMenuFragment;
 import messenger.ui.registration.R;
 
@@ -21,11 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        if(FirebaseAuth.getInstance().getCurrentUser()==null){
+            startActivities(new Intent[]{new Intent(MainActivity.this, AutorizationFragment.class)});
+        }
 
         replaceFragment(new ChatsMenuFragment());
     }
--- app/src/main/java/messenger/ui/ChatsMenuFragment.java
+++ app/src/main/java/messenger/ui/ChatsMenuFragment.java
@@ -19,9 +19,6 @@ public class ChatsMenuFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
     }
 
     @Override
