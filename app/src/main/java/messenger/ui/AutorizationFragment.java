7db49499c32da9c4151b0bed7fba69c4ef1a26d0
Motras <154556860+Motras06@users.noreply.github.com>
2024-09-04 00:39:29 +0300

    I WILL WORK IN THIS F*CKING SATURDAY

--- app/src/main/java/messenger/ui/AutorizationFragment.java
+++ app/src/main/java/messenger/ui/AutorizationFragment.java
@@ -9,6 +9,8 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -20,8 +22,10 @@ public class AutorizationFragment extends Fragment {
     public AutorizationFragment() {
     }
 
-    TextView textView;
-
+    EditText text_email;
+    EditText text_password;
+    Button button_go;
+    TextView reg;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,7 +36,10 @@ public class AutorizationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_autorization, container, false);
-        textView = (TextView) view.findViewById(R.id.go_to_reg);
+        text_email = (EditText) view.findViewById(R.id.mail_enter);
+        text_password = (EditText) view.findViewById(R.id.password_enter);
+        button_go = (Button) view.findViewById(R.id.button_enter);
+        reg = (TextView) view.findViewById(R.id.go_to_reg);
         return view;
     }
 }
